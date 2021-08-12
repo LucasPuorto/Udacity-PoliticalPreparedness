ab5d463c870fee301adc0086b58abd4d9af3205e
Lucas Puorto <lucas.puorto@br.experian.com>
2021-08-12 20:46:08 -0300

    set api key

--- starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
@@ -7,7 +7,7 @@ class CivicsHttpClient : OkHttpClient() {
 
     companion object {
 
-        const val API_KEY = "" //TODO: Place your API Key Here
+        const val API_KEY = "AIzaSyBpTxnysPJIDi8nfuJfNbsLsCbDX7JAb-0" //TODO: Place your API Key Here
 
         fun getClient(): OkHttpClient {
             return Builder()
